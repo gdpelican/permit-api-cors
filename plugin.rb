c76c5189e2975b115f7f8ab42c322f7dac225fc9
James Kiesel <james.kiesel@gmail.com>
2019-10-14 18:29:49 +1300

    Allow post controller to accept create submissions from API

--- plugin.rb
+++ plugin.rb
@@ -6,16 +6,23 @@
 # authors: James Kiesel (gdpelican)
 # url: https://github.com/gdpelican/permit-api-cors
 
-class ::Discourse::Cors
-  module AllowApiKey
-    HEADER_KEY = 'Access-Control-Allow-Headers'
-    HEADER_VALUES = ['Api-Key', 'Api-Username']
+after_initialize do
+  class ::Discourse::Cors
+    module AllowApiKey
+      HEADER_KEY = 'Access-Control-Allow-Headers'
+      HEADER_VALUES = ['Api-Key', 'Api-Username']
 
-    def apply_headers(cors_origins, env, headers)
-      result = super(cors_origins, env, headers)
-      result[HEADER_KEY] = result[HEADER_KEY].split(', ').concat(HEADER_VALUES).join(', ') if cors_origins.presence
-      result
+      def apply_headers(cors_origins, env, headers)
+        result = super(cors_origins, env, headers)
+        result[HEADER_KEY] = result[HEADER_KEY].split(', ').concat(HEADER_VALUES).join(', ') if cors_origins.presence
+        result
+      end
     end
+    singleton_class.prepend AllowApiKey
   end
-  singleton_class.prepend AllowApiKey
+
+  class ::PostsController
+    skip_before_action :verify_authenticity_token, only: :create
+    before_action :verify_authenticity_token, only: :create, if: :is_api?
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,4 @@ require './spec/rails_helper'
 path = "./plugins/permit-api-cors/plugin.rb"
 plugin = Plugin::Instance.new(Plugin::Metadata.parse(File.read(path)), path)
 plugin.activate!
-puts "HI!"
+plugin.initializers.first.call
